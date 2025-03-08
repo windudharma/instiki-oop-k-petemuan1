5f8d630175c2f93028355085bb882e73b23b5066
Windu Dharma <windudharma449@gmail.com>
2025-03-08 12:18:58 +0800

    Upload Project 1

---
+++ Pertemuan 1/src/pertemuan/pkg1/Pertemuan1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan.pkg1;
+
+/**
+ *
+ * @author windudharma
+ * TGL: 8 Maret 2025
+ */
+public class Pertemuan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Menampilkan Tulisan Dilayar
+        System.out.print("Huhahahahaha");
+
+
+    }
+  
+}
